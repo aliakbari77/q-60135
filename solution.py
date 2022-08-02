145db6eb574913da5669272c825e5399dae168d2
Ali Akbari <7731.aa@gmail.com>
2022-08-02 09:13:00 +0430

    extract link from url

--- solution.py
+++ solution.py
@@ -1,2 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
 def process(path):
-    pass
+    url = path
+    reqs = requests.get(url)
+    soup = BeautifulSoup(reqs.text, 'html.parser')
+
+    for link in soup.find_all('a'):
+        print(link.get('href'))
+
+process("https://www.quera.org/")
