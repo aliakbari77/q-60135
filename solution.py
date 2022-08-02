8b7e954c8afcb2e4142cddf115e700ec69afcf4b
Ali Akbari <7731.aa@gmail.com>
2022-08-02 09:25:44 +0430

    extract links from file

--- solution.py
+++ solution.py
@@ -1,12 +1,27 @@
-import requests
-from bs4 import BeautifulSoup
+# import requests
+# from bs4 import BeautifulSoup
+
+# def process(path):
+#     url = path
+#     reqs = requests.get(url)
+#     soup = BeautifulSoup(reqs.text, 'html.parser')
+
+#     for link in soup.find_all('a'):
+#         print(link.get('href'))
+
+# process("https://www.quera.org/")
+
+import re
 
 def process(path):
-    url = path
-    reqs = requests.get(url)
-    soup = BeautifulSoup(reqs.text, 'html.parser')
+    with open(path) as f:
+        urls = f.read()
+        links = re.findall('"((http)s?://.*?)"', urls)
+        result = []
+    for url in links:
+        result.append(url[0])
+    
+    return result
 
-    for link in soup.find_all('a'):
-        print(link.get('href'))
+print(process("htmlsampletest2.html"))
 
-process("https://www.quera.org/")
